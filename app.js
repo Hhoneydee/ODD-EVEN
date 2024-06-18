6201cd64b65043e8570c23cdaa3a3796ab51ff9b
Hhoneydee <113873556+Hhoneydee@users.noreply.github.com>
2024-06-18 19:35:22 +0400

    first conditional statement

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const Number = 25
+const side1 = 5
+const side2 = 5
+const side3 = 5
 
-console.log(Number % 2 === 0)
 
-if(Number%2){
-    console.log("Number is even")
-}else{
-console.log("Number is odd")
-}
+
+ if(side1 === side2 === side3){
+    console.log("Equilateral triangle")
+ }else if(side1 === side2 || side1 === side3 || side2 === side3){
+    console.log("Isosceles triangle")
+ }else
+ console.log("Scalene triangle")
